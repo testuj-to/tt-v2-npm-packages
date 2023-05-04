0f31dae8b4bb1d42b429c3e45a02cade5f78c4e9
petr.hulinsky <petr.hulinsky@koala42.com>
2023-05-04 13:47:16 +0200

    ✨ Secondary button

--- packages/@testuj.to/react/src/utils/cssifyTheme.ts
+++ packages/@testuj.to/react/src/utils/cssifyTheme.ts
@@ -1,106 +1,108 @@
-
-import { TenantTheme } from '@lib/types'
+import { TenantTheme } from "@lib/types";
 
 export interface ThemeCSS {
-    readonly cssVariables: {
-        variables: {
-            name: string
-            value: string
-        }[]
-        toString(): string
-    }
-    readonly cssFontImports: {
-        imports: string[]
-        toString(): string
-    }
-    readonly cssFontFaces: {
-        fontFaces: {
-            family: string
-            src: string
-            style?: string
-            weight?: string
-        }[]
-        toString(): string
-    }
-    readonly cssGlobal: string
+  readonly cssVariables: {
+    variables: {
+      name: string;
+      value: string;
+    }[];
+    toString(): string;
+  };
+  readonly cssFontImports: {
+    imports: string[];
+    toString(): string;
+  };
+  readonly cssFontFaces: {
+    fontFaces: {
+      family: string;
+      src: string;
+      style?: string;
+      weight?: string;
+    }[];
+    toString(): string;
+  };
+  readonly cssGlobal: string;
 }
 
 export const cssifyTheme = (theme: TenantTheme): ThemeCSS => {
-    const themeCSS: ThemeCSS = {
-        ...theme,
-        get cssVariables() {
-            const variables = []
+  const themeCSS: ThemeCSS = {
+    ...theme,
+    get cssVariables() {
+      const variables = [];
 
-            const addVariable = (name: string, value: string) => {
-                if (value) {
-                    variables.push({ name, value })
-                }
-            }
+      const addVariable = (name: string, value: string) => {
+        if (value) {
+          variables.push({ name, value });
+        }
+      };
 
-            addVariable('--color-primary',       theme?.colorScheme?.primary)
-            addVariable('--color-secondary',     theme?.colorScheme?.secondary)
-            addVariable('--color-text',          theme?.colorScheme?.text)
-            addVariable('--color-link',          theme?.colorScheme?.link)
-            addVariable('--color-button',        theme?.colorScheme?.button)
-            addVariable('--color-button-bg',     theme?.colorScheme?.buttonBg)
-            addVariable('--color-error',         theme?.colorScheme?.error)
-            addVariable('--color-categories-bg', theme?.colorScheme?.categoriesBg)
-            addVariable('--font-text',           theme?.fontFamily?.text)
-            addVariable('--font-heading',        theme?.fontFamily?.heading)
-            addVariable('--border-radius',       theme?.borderRadius)
+      addVariable("--color-primary", theme?.colorScheme?.primary);
+      addVariable("--color-secondary", theme?.colorScheme?.secondary);
+      addVariable("--color-text", theme?.colorScheme?.text);
+      addVariable("--color-link", theme?.colorScheme?.link);
+      addVariable("--color-button", theme?.colorScheme?.button);
+      addVariable("--color-button-bg", theme?.colorScheme?.buttonBg);
+      addVariable("--color-button-secondary-bg", theme?.colorScheme?.buttonSecBg);
+      addVariable("--color-button-secondary-text", theme?.colorScheme?.buttonSecText);
+      addVariable("--color-button-secondary-border", theme?.colorScheme?.buttonSecBorder);
+      addVariable("--color-button-secondary-border-hover", theme?.colorScheme?.buttonSecBorderHov);
+      addVariable("--color-error", theme?.colorScheme?.error);
+      addVariable("--color-categories-bg", theme?.colorScheme?.categoriesBg);
+      addVariable("--font-text", theme?.fontFamily?.text);
+      addVariable("--font-heading", theme?.fontFamily?.heading);
+      addVariable("--border-radius", theme?.borderRadius);
 
-
-            return {
-                variables,
-                toString() {
-                    return `\n${variables.map(({ name, value }) => `  ${name}: ${value};`).join('\n')}\n`
-                },
-            }
+      return {
+        variables,
+        toString() {
+          return `\n${variables.map(({ name, value }) => `  ${name}: ${value};`).join("\n")}\n`;
         },
-        get cssFontImports() {
-            const imports = (theme?.fontFamily?.imports || []).map(_import => {
-                return _import?.statement
-            })
+      };
+    },
+    get cssFontImports() {
+      const imports = (theme?.fontFamily?.imports || []).map((_import) => {
+        return _import?.statement;
+      });
 
-            return {
-                imports,
-                toString() {
-                    return imports.map(_import => `  ${_import}\n`).join('')
-                },
-            }
+      return {
+        imports,
+        toString() {
+          return imports.map((_import) => `  ${_import}\n`).join("");
         },
-        get cssFontFaces() {
-            const fontFaces = (theme?.fontFamily?.fontFaces || []).map(fontFace => {
-                return {
-                    ...fontFace,
-                    toString() {
-                        const properties = [
-                            `    font-family: ${fontFace.family};`,
-                            `    src: ${fontFace.src};`,
-                        ]
+      };
+    },
+    get cssFontFaces() {
+      const fontFaces = (theme?.fontFamily?.fontFaces || []).map((fontFace) => {
+        return {
+          ...fontFace,
+          toString() {
+            const properties = [
+              `    font-family: ${fontFace.family};`,
+              `    src: ${fontFace.src};`,
+            ];
 
-                        if (fontFace.style) {
-                            properties.push(`    font-style: ${fontFace.style};`)
-                        }
-
-                        if (fontFace.weight) {
-                            properties.push(`    font-weight: ${fontFace.weight};`)
-                        }
-
-                        return `  @font-face{\n${properties.join('\n')}\n  }\n`
-                    },
-                }
-            })
-
-            return {
-                fontFaces,
-                toString() {
-                    return fontFaces.map(fontFace => fontFace.toString()).join('\n')
-                },
+            if (fontFace.style) {
+              properties.push(`    font-style: ${fontFace.style};`);
             }
+
+            if (fontFace.weight) {
+              properties.push(`    font-weight: ${fontFace.weight};`);
+            }
+
+            return `  @font-face{\n${properties.join("\n")}\n  }\n`;
+          },
+        };
+      });
+
+      return {
+        fontFaces,
+        toString() {
+          return fontFaces.map((fontFace) => fontFace.toString()).join("\n");
         },
-        get cssGlobal() {
-            return `
+      };
+    },
+    get cssGlobal() {
+      return `
 ${themeCSS?.cssFontImports?.toString?.()}
 ${themeCSS?.cssFontFaces?.toString?.()}
 
@@ -146,9 +148,9 @@ h6 {
         background: black;
     }
 } */
-`
-        },
-    }
+`;
+    },
+  };
 
-    return themeCSS
-}
+  return themeCSS;
+};
