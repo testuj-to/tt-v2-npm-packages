d2b2adc064cb8e1dda8d20b4d741080c6d666298
petr.hulinsky <petr.hulinsky@koala42.com>
2023-05-10 09:26:47 +0200

    💄 Update Checkbox component

--- packages/@testuj.to/react/src/components/Checkbox/index.tsx
+++ packages/@testuj.to/react/src/components/Checkbox/index.tsx
@@ -1,45 +1,39 @@
+import { useCallback, useState } from "react";
+import { Root, Indicator } from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
 
-import { useCallback, useState } from 'react'
-import { Root, Indicator } from '@radix-ui/react-checkbox'
-import { CheckIcon } from '@radix-ui/react-icons'
-
-import './styles.css'
+import "./styles.css";
 
 export interface CheckboxProps {
-    value: boolean
-    label?: string
-    onChange?(value: boolean)
+  value: boolean;
+  label?: string;
+  onChange?(value: boolean);
 }
 
 export const Checkbox = ({ value, label, onChange }: CheckboxProps) => {
-    const [ id ] = useState(Math.random().toString(36).substring(2))
+  const [id] = useState(Math.random().toString(36).substring(2));
 
-    const handleChange = useCallback((isChecked: boolean|string) => {
-        if (typeof onChange === 'function') {
-            onChange(!!isChecked)
-        }
-    }, [ onChange ])
+  const handleChange = useCallback(
+    (isChecked: boolean | string) => {
+      if (typeof onChange === "function") {
+        onChange(!!isChecked);
+      }
+    },
+    [onChange]
+  );
 
-    return (
-        <div className='tt-checkbox-container'>
-            <Root
-                id={id}
-                className='tt-checkbox'
-                checked={value}
-                onCheckedChange={handleChange}
-            >
-                <Indicator className='tt-checkbox-indicator'>
-                    <CheckIcon/>
-                </Indicator>
-            </Root>
-            {label && (
-                <label
-                    htmlFor={id}
-                    className='tt-checkbox-label'
-                >
-                    {label}
-                </label>
-            )}
-        </div>
-    )
-}
+  return (
+    <div className="tt-checkbox-container">
+      <Root id={id} className="tt-checkbox" checked={value} onCheckedChange={handleChange}>
+        <Indicator className="tt-checkbox-indicator">
+          <CheckIcon className="tt-checkbox-checkmark" />
+        </Indicator>
+      </Root>
+      {label && (
+        <label htmlFor={id} className="tt-checkbox-label">
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
