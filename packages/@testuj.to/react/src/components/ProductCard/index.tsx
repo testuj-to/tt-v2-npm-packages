14e377f4e781d3ec8f630ac12b7d4bfcc296413a
petr.hulinsky <petr.hulinsky@koala42.com>
2023-05-23 15:49:10 +0200

    🐛 Fix DatePicker Forward ref error

--- packages/@testuj.to/react/src/components/ProductCard/index.tsx
+++ packages/@testuj.to/react/src/components/ProductCard/index.tsx
@@ -1,4 +1,3 @@
-import { Card } from "../Card";
 import { Tag, TagProps } from "../Tag";
 import "./styles.css";
 
@@ -8,16 +7,17 @@ export interface ProductCardProps {
   image: React.ReactNode; // image is passed as react node to allow usage of optimized Image from next.js
   label: string;
   tags: ProductCardTagProps[];
+  onClick?: () => void;
 }
 
-export const ProductCard = ({ image, label, tags }: ProductCardProps) => {
+export const ProductCard = ({ image, label, tags, onClick }: ProductCardProps) => {
   return (
-    <div className="tt-product-card-warpper">
+    <div className="tt-product-card-warpper" onClick={() => onClick?.()}>
       <div className="tt-product-card-main">
         <div className="tt-product-card-image">{image}</div>
         <div className="tt-product-card-tags">
-          {tags?.map((tag) => (
-            <Tag {...tag} className="tt-product-card-tag" />
+          {tags?.map((tag, index) => (
+            <Tag {...tag} key={index} className="tt-product-card-tag" />
           ))}
         </div>
       </div>
