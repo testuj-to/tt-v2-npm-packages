969b3028faddb53af367c7ef710974a5586c48c8
petr.hulinsky <petr.hulinsky@koala42.com>
2023-06-09 14:11:08 +0200

    ✨ User Menu

--- packages/@testuj.to/react/src/components/UserMenu/UserMenu.stories.tsx
+++ packages/@testuj.to/react/src/components/UserMenu/UserMenu.stories.tsx
@@ -15,4 +15,37 @@ export default {
 const userMenuStoryTemplate: ComponentStory<typeof UserMenu> = (args) => <UserMenu {...args} />;
 
 export const Basic = userMenuStoryTemplate.bind({});
-Basic.args = {};
+Basic.args = {
+  user: {
+    image: "https://picsum.photos/40/40",
+    name: "Jan Novák",
+    title: "Super Tester",
+    badge: "https://picsum.photos/18/18",
+  },
+  xpBar: {
+    value: 65,
+    breakpoints: [20, 40, 60, 80],
+  },
+  menuItems: [
+    {
+      label: "Upravit osobní údaje",
+      onClick: () => console.log("Upravit osobní údaje"),
+    },
+    {
+      label: "Změnit heslo",
+      onClick: () => console.log("Změnit heslo"),
+    },
+    {
+      label: "Změnit profilovou fotografii",
+      onClick: () => console.log("Změnit profilovou fotografii"),
+    },
+    {
+      label: "Preference",
+      onClick: () => console.log("Preference"),
+    },
+    {
+      label: "Odhlásit se",
+      onClick: () => console.log("Odhlásit se"),
+    },
+  ],
+};
--- packages/@testuj.to/react/src/components/UserMenu/index.tsx
+++ packages/@testuj.to/react/src/components/UserMenu/index.tsx
@@ -1,5 +1,61 @@
+import * as Popover from "@radix-ui/react-popover";
+import cx from "classnames";
 import "./styles.css";
+import { UserAvatar } from "../UserAvatar";
+import { ProgressBar } from "../ProgressBar";
 
-export const UserMenu = (props) => {
-  return <div></div>;
-};
+export interface User {
+  name: string;
+  title: string;
+  image: string;
+  badge?: string;
+}
+
+export interface MenuItem {
+  label: string;
+  onClick: () => void;
+}
+
+export interface UserMenuProps {
+  user: User;
+  menuItems?: MenuItem[];
+  xpBar?: {
+    value?: number;
+    hidden?: boolean;
+    breakpoints?: number[];
+  };
+  className?: string;
+}
+
+export const UserMenu = ({ user, xpBar, menuItems, className }: UserMenuProps) => (
+  <Popover.Root>
+    <Popover.Trigger asChild>
+      <button className={cx("tt-user-menu_trigger", className)}>
+        <UserAvatar {...user} hideUserText />
+      </button>
+    </Popover.Trigger>
+    <Popover.Portal>
+      <Popover.Content className="tt-user-menu_content" sideOffset={5}>
+        <div className="tt-user-menu_header">
+          <UserAvatar {...user} />
+          {xpBar?.hidden ? null : (
+            <ProgressBar
+              className="tt-user-menu_progress-bar"
+              value={xpBar?.value}
+              breakpoints={xpBar.breakpoints}
+              delay={200}
+              breakpointColor="var(--color-text-secondary)"
+            />
+          )}
+        </div>
+        <div className="tt-user-menu_body">
+          {menuItems?.map(({ label, onClick }, index) => (
+            <div key={label + index} {...onClick} className="tt-user-menu_item">
+              {label}
+            </div>
+          ))}
+        </div>
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>
+);
