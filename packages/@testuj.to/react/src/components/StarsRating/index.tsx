b1fd6f3cf5917355ac40b5ca8363cf428fdecc8f
petr.hulinsky <petr.hulinsky@koala42.com>
2023-06-14 12:25:44 +0200

    ✨ Product card info

--- packages/@testuj.to/react/src/components/StarsRating/index.tsx
+++ packages/@testuj.to/react/src/components/StarsRating/index.tsx
@@ -6,15 +6,23 @@ export interface StarsRatingProps {
   rating: number;
   onChange: (rating: number) => void;
   readOnly?: boolean;
+  className?: string;
+  starSize?: number;
 }
 
-export const StarsRating = ({ rating, onChange, readOnly }: StarsRatingProps) => {
+export const StarsRating = ({
+  rating,
+  onChange,
+  readOnly,
+  className,
+  starSize,
+}: StarsRatingProps) => {
   const [ratingHover, setRatingHover] = useState<number | null>(null);
   const ratingInt = ratingHover || Math.floor(rating);
   const ratingDecimal = (ratingHover || rating) - ratingInt;
 
   return (
-    <div>
+    <div className={className}>
       {[1, 2, 3, 4, 5].map((filled) => {
         const filledPercent =
           filled <= ratingInt ? 100 : filled > ratingInt + 1 ? 0 : ratingDecimal * 100;
@@ -27,6 +35,8 @@ export const StarsRating = ({ rating, onChange, readOnly }: StarsRatingProps) =>
             setGradient={filled > ratingInt}
             onMouseEnter={() => !readOnly && setRatingHover(filled)}
             onMouseLeave={() => !readOnly && setRatingHover(null)}
+            height={starSize || 24}
+            width={starSize || 24}
           />
         );
       })}
