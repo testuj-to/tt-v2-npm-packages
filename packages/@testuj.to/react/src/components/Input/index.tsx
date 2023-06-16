258cbf4a233e091e9f8423860fd2dea45e70c046
petr.hulinsky <petr.hulinsky@koala42.com>
2023-06-16 12:25:14 +0200

    ✨ Input icons

--- packages/@testuj.to/react/src/components/Input/index.tsx
+++ packages/@testuj.to/react/src/components/Input/index.tsx
@@ -1,18 +1,29 @@
+import React, { InputHTMLAttributes } from "react";
+import cx from "classnames";
 
-import React, { type InputHTMLAttributes } from 'react'
-import cx from 'classnames'
-
-import './styles.css'
+import "./styles.css";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    variant?: 'default' | 'dark'
+  variant?: "default" | "dark";
+  icon?: React.ReactNode;
+  iconClassName?: string;
 }
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({variant, className, ...rest }, innerRef) => {
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ variant, className, icon, iconClassName, ...rest }, innerRef) => {
     return (
+      <div className="tt-input-wrapper">
+        {icon && <div className={cx("tt-input-icon", iconClassName)}>{icon}</div>}
         <input
-            ref={innerRef}
-            {...rest}
-            className={cx('tt-input', {"tt-input-dark": variant === "dark"} ,className)}
+          ref={innerRef}
+          {...rest}
+          className={cx(
+            "tt-input",
+            { "tt-input-dark": variant === "dark" },
+            { icon: icon },
+            className
+          )}
         />
-    )
-})
+      </div>
+    );
+  }
+);
