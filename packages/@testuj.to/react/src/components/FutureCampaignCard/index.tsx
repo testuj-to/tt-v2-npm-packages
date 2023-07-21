d7d3070127ec760d7aabbf20887a12cc61e22df6
petr.hulinsky <petr.hulinsky@koala42.com>
2023-07-21 14:46:30 +0200

    ✨ Future Campaing Card

--- packages/@testuj.to/react/src/components/FutureCampaignCard/index.tsx
+++ packages/@testuj.to/react/src/components/FutureCampaignCard/index.tsx
@@ -1,5 +1,34 @@
-export const FutureCampaignCard = () => {
-    return <div>
-        
-    </div>
+import { useMemo } from "react";
+import { CampaignCard, CampaignCardTagProps } from "../CampaignCard";
+import { Campaign } from "@lib/types";
+
+export interface FutureCampaignCardProps {
+  campaign: Campaign;
+  onClick?: () => void;
 }
+
+export const FutureCampaignCard = ({ campaign, onClick }: FutureCampaignCardProps) => {
+  // TODO: generate tags based on campaign state
+  const tags: CampaignCardTagProps[] = useMemo(() => {
+    return [
+      {
+        children: "👌 40% SLEVA",
+        variant: "white",
+      },
+      {
+        children: "Probíhá přihlašování",
+        variant: "warning",
+        color: "#FFC107",
+      },
+    ];
+  }, []);
+
+  return (
+    <CampaignCard
+      image={<img src="https://picsum.photos/384/264" alt="img" />}
+      label="Product title"
+      tags={tags}
+      onClick={onClick}
+    />
+  );
+};
