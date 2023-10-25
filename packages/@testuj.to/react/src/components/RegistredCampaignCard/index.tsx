55ba9ff19802e4644eda54e2661ac5238cb90a95
petr.hulinsky <petr.hulinsky@koala42.com>
2023-10-25 09:48:31 +0200

    fix: add black for late reviews

--- packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
+++ packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
@@ -78,6 +78,7 @@ export const RegistredCampaignCard = ({
             return {
               children: t("cardStatus.missingReview", { count: daysPastReviewsEnd }),
               variant: "danger",
+              color: "#000000",
             };
           }
 
