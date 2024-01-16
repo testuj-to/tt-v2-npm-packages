76919e53dd2f0f30c579a17944d29c57c3038f54
Petr Hulínský <phulinsky@gmail.com>
2024-01-16 19:24:34 +0100

    feat: add half star rating to star rating component

--- packages/@testuj.to/react/src/components/StarsRating/index.tsx
+++ packages/@testuj.to/react/src/components/StarsRating/index.tsx
@@ -3,42 +3,49 @@ import { StarIcon } from "./StarIcon";
 import "./styles.css";
 
 export interface StarsRatingProps {
-  rating: number;
-  onChange?: (rating: number) => void;
-  readOnly?: boolean;
-  className?: string;
-  starSize?: number;
+    rating: number;
+    onChange?: (rating: number) => void;
+    readOnly?: boolean;
+    className?: string;
+    starSize?: number;
+    noOfStars?: number;
 }
 
-export const StarsRating = ({
-  rating,
-  onChange,
-  readOnly,
-  className,
-  starSize,
-}: StarsRatingProps) => {
-  const [ratingHover, setRatingHover] = useState<number | null>(null);
-  const ratingInt = ratingHover || Math.floor(rating);
-  const ratingDecimal = (ratingHover || rating) - ratingInt;
+export const StarsRating = ({ rating, onChange, readOnly, className, starSize, noOfStars = 5 }: StarsRatingProps) => {
+    const [ratingHover, setRatingHover] = useState<number | null>(null);
+    const ratingInt = Math.floor(ratingHover) || Math.floor(rating);
+    const ratingDecimal = (ratingHover || rating) - ratingInt;
 
-  return (
-    <div className={className}>
-      {[1, 2, 3, 4, 5].map((star) => {
-        const starPercent =
-          star <= ratingInt ? 100 : star > ratingInt + 1 ? 0 : ratingDecimal * 100;
-        return (
-          <StarIcon
-            key={star}
-            filled={starPercent}
-            onClick={() => !readOnly && onChange(star)}
-            setGradient={star > ratingInt}
-            onMouseEnter={() => !readOnly && setRatingHover(star)}
-            onMouseLeave={() => !readOnly && setRatingHover(null)}
-            height={starSize || 24}
-            width={starSize || 24}
-          />
-        );
-      })}
-    </div>
-  );
+    const stars = new Array(noOfStars).fill(0).map((_, i) => i + 1);
+
+    return (
+        <div className={className}>
+            {stars.map((star) => {
+                const starPercent = star <= ratingInt ? 100 : star > ratingInt + 1 ? 0 : ratingDecimal * 100;
+                return (
+                    <div className="tt-stars-rating-item">
+                        <StarIcon
+                            key={star}
+                            filled={starPercent}
+                            setGradient={star > ratingInt}
+                            height={starSize || 24}
+                            width={starSize || 24}
+                        />
+                        <div
+                            className="tt-stars-rating-item__overlay_left"
+                            onClick={() => !readOnly && onChange(star - 0.5)}
+                            onMouseEnter={() => !readOnly && setRatingHover(star - 0.5)}
+                            onMouseLeave={() => !readOnly && setRatingHover(null)}
+                        />
+                        <div
+                            className="tt-stars-rating-item__overlay_right"
+                            onClick={() => !readOnly && onChange(star)}
+                            onMouseEnter={() => !readOnly && setRatingHover(star)}
+                            onMouseLeave={() => !readOnly && setRatingHover(null)}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
