419b604b431e5313eeb35a98fb82870308471d8f
Petr Hulínský <phulinsky@gmail.com>
2024-01-19 12:14:38 +0100

    fix: RatingBarChart

--- packages/@testuj.to/react/src/components/Checkbox/Checkbox.stories.tsx
+++ packages/@testuj.to/react/src/components/Checkbox/Checkbox.stories.tsx
@@ -4,18 +4,22 @@ import { TTContextDecorator } from "../../context.stories";
 import { Checkbox } from ".";
 
 export default {
-  component: Checkbox,
-  title: "Primitives/Checkbox",
-  parameters: {
-    layout: "centered",
-  },
-  decorators: [TTContextDecorator],
+    component: Checkbox,
+    title: "Primitives/Checkbox",
+    parameters: {
+        layout: "centered",
+    },
+    decorators: [TTContextDecorator],
 } as ComponentMeta<typeof Checkbox>;
 
-const checkboxStory: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />;
+const checkboxStory: ComponentStory<typeof Checkbox> = (args) => (
+    <div style={{ maxWidth: "200px" }}>
+        <Checkbox {...args} />
+    </div>
+);
 
 export const CheckboxStory = checkboxStory.bind({});
 CheckboxStory.args = {
-  label: "I'm a label!",
-  variant: "framed",
+    label: "Ahoj já jsem velmi dloyhý text bla bla bla, Ahoj já jsem velmi dloyhý text bla bla bla ",
+    variant: "outlined",
 };
--- packages/@testuj.to/react/src/components/RatingBarChart/index.tsx
+++ packages/@testuj.to/react/src/components/RatingBarChart/index.tsx
@@ -5,73 +5,74 @@ import { StarIcon } from "./StarIcon";
 
 import "./styles.css";
 export interface RatingBarChartItem {
-  label: string;
-  value: number;
-  icon?: React.ReactNode;
+    label: string;
+    value: number;
+    icon?: React.ReactNode;
 }
 
 export interface RatingBarChartProps {
-  className?: string;
-  items: RatingBarChartItem[];
-  delay?: boolean;
+    className?: string;
+    items: RatingBarChartItem[];
+    delay?: boolean;
 }
 
 export const RatingBarChart = ({ items, className, delay }: RatingBarChartProps) => {
-  const max = Math.max(...items.map((item) => item.value));
+    const max = Math.max(...items.map((item) => item.value));
 
-  return (
-    <div className={cx("tt-rating-bar-chart", className)}>
-      {items.map(({ label, value, icon }, index) => {
-        return (
-          <RatingBarChartItem
-            key={index}
-            {...{ label, icon, value, delay }}
-            width={(value / max) * 100}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className={cx("tt-rating-bar-chart", className)}>
+            {items.map(({ label, value, icon }, index) => {
+                return (
+                    <RatingBarChartItem key={index} {...{ label, icon, value, delay }} width={(value / max) * 100} />
+                );
+            })}
+        </div>
+    );
 };
 
 export interface RatingBarChartItemProps {
-  label: string;
-  width: number;
-  value?: number;
-  icon?: React.ReactNode;
-  delay?: boolean;
+    label: string;
+    width: number;
+    value?: number;
+    icon?: React.ReactNode;
+    delay?: boolean;
 }
 
 const finalWidth = (width: number) => {
-  if (width === 0) {
-    return 0;
-  }
-  if (width < 5) {
-    return 5;
-  }
-  if (width > 100) {
-    return 100;
-  }
-  return width;
+    if (width === 0) {
+        return 0;
+    }
+    if (width < 5) {
+        return 5;
+    }
+    if (width > 100) {
+        return 100;
+    }
+    return width;
 };
 
 export const RatingBarChartItem = ({ label, width, value, icon }: RatingBarChartItemProps) => {
-  return (
-    <div className="tt-rating-bar-chart__item">
-      <div className="tt-rating-bar-chart__item__label">{label}</div>
-      <div className="tt-rating-bar-chart__item__icon">{icon || <StarIcon />}</div>
-      <div className="tt-rating-bar-chart__item__bar">
-        <div
-          className="tt-rating-bar-chart__item__bar__fill"
-          style={{ width: `${finalWidth(width)}%` }}
-        ></div>
-        <div
-          className="tt-rating-bar-chart__item__bar__label"
-          style={{ paddingLeft: `${width === 0 ? 8 : 0}px` }}
-        >
-          {value}
+    const [widthState, setWidthState] = useState(0);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setWidthState(width);
+        }, 600);
+    }, [width]);
+
+    return (
+        <div className="tt-rating-bar-chart__item">
+            <div className="tt-rating-bar-chart__item__label">{label}</div>
+            <div className="tt-rating-bar-chart__item__icon">{icon || <StarIcon />}</div>
+            <div className="tt-rating-bar-chart__item__bar">
+                <div className="tt-rating-bar-chart__item__bar__fill" style={{ width: `${finalWidth(widthState)}%` }}></div>
+                <div
+                    className="tt-rating-bar-chart__item__bar__label"
+                    style={{ paddingLeft: `${width === 0 ? 8 : 0}px` }}
+                >
+                    {value}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
