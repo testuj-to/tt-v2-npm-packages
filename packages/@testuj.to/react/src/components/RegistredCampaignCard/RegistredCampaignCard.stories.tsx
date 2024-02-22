2938cb18d3c285e28cf718ee462f99da4428fc28
Petr Hulínský <phulinsky@gmail.com>
2024-02-22 12:50:58 +0100

    fix: RegistredCampaignCard tags

--- packages/@testuj.to/react/src/components/RegistredCampaignCard/RegistredCampaignCard.stories.tsx
+++ packages/@testuj.to/react/src/components/RegistredCampaignCard/RegistredCampaignCard.stories.tsx
@@ -4,72 +4,140 @@ import { TTContextDecorator } from "../../context.stories";
 import { RegistredCampaignCard } from ".";
 
 export default {
-  component: RegistredCampaignCard,
-  title: "Composites/RegistredCampaignCard",
-  parameters: {
-    layout: "centered",
-  },
-  decorators: [TTContextDecorator],
+    component: RegistredCampaignCard,
+    title: "Composites/RegistredCampaignCard",
+    parameters: {
+        layout: "centered",
+    },
+    decorators: [TTContextDecorator],
 } as ComponentMeta<typeof RegistredCampaignCard>;
 
 const registredcampaignCardStoryTemplate: ComponentStory<typeof RegistredCampaignCard> = (args) => (
-  <RegistredCampaignCard {...args} />
+    <RegistredCampaignCard {...args} />
 );
 
 export const Basic = registredcampaignCardStoryTemplate.bind({});
 Basic.args = {
-  image: <img src="https://picsum.photos/384/264" alt="img" />,
-  label: "Product title",
-  tags: [
-    {
-      children: "👌 40% SLEVA",
-      variant: "white",
-    },
-    {
-      children: "Probíhá přihlašování",
-      variant: "warning",
-      color: "#FFC107",
-    },
-  ],
-  onClick: () => console.log("Product card clicked"),
-  info: {
-    rating: Math.random() * 5,
-    noOfReviews: Math.floor(Math.floor(Math.random() * 100)),
-    date: new Date().toISOString(),
-  },
-  showInfo: true,
-  translations: {
-    review: "recenze",
-    reviews: "recenzí",
-  },
-  campaign: {
-    settings: {
-      // openAt: today - 1 week as iso string
-      // openAt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
-      openAt: 1697320800000,
-      registrationPeriodDays: 14,
-      submissionPeriodDays: 3,
-    },
-    campaignApplication: {
-      state: "resolved",
-      submittedAt: null,
-      isDelivered: true,
-      id: "2X9yAmK0ffaw2HxhCULVssFoCAT",
-      discountCode: null,
-      deliveryConfirmedAt: 1698058333514,
-      questionnaireResponse: null,
-      latestResolution: {
-        message: null,
-        resolvedAt: 1698055585172,
-        status: "selected",
-      },
-    },
-    reviews: [
-      {
-        state: "draft",
-        id: "2X9yAmK0ffaw2HxhCULVssFoCAT",
-      },
+    image: <img src="https://test.btcdn.org/img/38875a29b811476d980eaba3d64b3a9e.png" alt="img" />,
+    label: "Product title",
+    tags: [
+        {
+            children: "👌 40% SLEVA",
+            variant: "white",
+        },
+        {
+            children: "Probíhá přihlašování",
+            variant: "warning",
+            color: "#FFC107",
+        },
     ],
-  },
-  t: (key: string, args?: any) => key,
+    onClick: () => console.log("Product card clicked"),
+    info: {
+        rating: Math.random() * 5,
+        noOfReviews: Math.floor(Math.floor(Math.random() * 100)),
+        date: new Date().toISOString(),
+    },
+    showInfo: true,
+    translations: {
+        review: "recenze",
+        reviews: "recenzí",
+    },
+    campaign: {
+        id: "2c28gUzlfkPkZKpO8xW75uHSp5t",
+        state: "closed",
+        slug: "timeline",
+        name: "Timeline",
+        thumbnailImage: {
+            src: "https://test.btcdn.org/img/38875a29b811476d980eaba3d64b3a9e.png",
+        },
+        images: [
+            {
+                file: {
+                    id: "38875a29b811476d980eaba3d64b3a9e",
+                    src: "https://test.btcdn.org/img/38875a29b811476d980eaba3d64b3a9e.png",
+                },
+                isThumbnail: null,
+            },
+        ],
+        campaignApplication: {
+            id: "2c2FnisbWzmqwuvxsbxXO2G8rbU",
+            state: "resolved",
+            submittedAt: null,
+            isDelivered: null,
+            latestResolution: {
+                status: "selected",
+                resolvedAt: 1707305067017,
+                message: null,
+            },
+        },
+        reviews: [],
+        products: [
+            {
+                category: {
+                    key: "health-and-beauty",
+                },
+                id: "2ZMok7mHmh9u8eFrj6ZVawWZzl7",
+                image: {
+                    src: "https://test.btcdn.org/img/b080ae2fee6744a89f85d0ff17911464.png",
+                    id: "b080ae2fee6744a89f85d0ff17911464",
+                },
+                info: {
+                    manufacturer: null,
+                    model: null,
+                },
+                name: "Aplikace KucKuc",
+                urlAddress: null,
+            },
+            {
+                category: {
+                    key: "health-and-beauty",
+                },
+                id: "2Vz3hbcnhyLAVxtjUjeN7mswYcn",
+                image: {
+                    src: "https://test.btcdn.org/p/2Vz3hbcnhyLAVxtjUjeN7mswYcn/354dcd9e-3e26-47b8-aeed-6a6cdca006eb.jpg",
+                    id: "354dcd9e-3e26-47b8-aeed-6a6cdca006eb",
+                },
+                info: {
+                    manufacturer: {
+                        logo: null,
+                        id: "2WVfX439q7PcISeL13Ui0E8iey6",
+                        name: "Krémy.cz",
+                    },
+                    model: null,
+                },
+                name: "Opalovací krém",
+                urlAddress: null,
+            },
+            {
+                category: {
+                    key: "health-and-beauty",
+                },
+                id: "2Wic0Wq7ZoVJMewfpkyr2LYNKSl",
+                image: {
+                    src: "https://test.btcdn.org/p/2Wic0Wq7ZoVJMewfpkyr2LYNKSl/e12485bb-0b8f-41a8-886c-f379e2c07ade.jpeg",
+                    id: "e12485bb-0b8f-41a8-886c-f379e2c07ade",
+                },
+                info: {
+                    manufacturer: {
+                        logo: null,
+                        id: "2WVfX2jWIvdn2VCKMdfECdPtgfO",
+                        name: "Orel",
+                    },
+                    model: "Jedlý",
+                },
+                name: "Vincentka",
+                urlAddress: null,
+            },
+        ],
+        settings: {
+            capacity: 4,
+            openAt: 1707300000000,
+            registrationPeriodDays: 2,
+            submissionPeriodDays: 1,
+            closeAt: 1707523200000,
+        },
+        tags: [],
+        tenantLogo: null,
+    },
+    t: (key: string, args?: any) => key,
 };
--- packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
+++ packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
@@ -1,158 +1,53 @@
 import { useMemo, useState } from "react";
 import { CampaignCard, CampaignCardTagProps } from "../CampaignCard";
-import {
-  Campaign,
-  CampaignApplicationResolutionStatus,
-  CampaignApplicationStatus,
-  ReviewState,
-} from "@lib/types";
+import { Campaign, CampaignApplicationResolutionStatus, CampaignApplicationStatus, ReviewState } from "@lib/types";
+import { useGenerateTag } from "./useGenerateTag";
 
 export interface RegistredCampaignCardProps {
-  campaign: Campaign;
-  onClick?: () => void;
-  onDoubleClick?: () => void;
-  image?: React.ReactNode;
-  liked?: boolean;
-  onLikeClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  t: (key: string, args?: any) => string;
+    campaign: Campaign;
+    onClick?: () => void;
+    onDoubleClick?: () => void;
+    image?: React.ReactNode;
+    liked?: boolean;
+    onLikeClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    t: (key: string, args?: any) => string;
 }
 
 export const RegistredCampaignCard = ({
-  campaign,
-  onClick,
-  image,
-  onDoubleClick,
-  liked,
-  onLikeClick,
-  t,
+    campaign,
+    onClick,
+    image,
+    onDoubleClick,
+    liked,
+    onLikeClick,
+    t,
 }: RegistredCampaignCardProps) => {
-  const campaignApplication = campaign?.campaignApplication;
+    const generatedTag = useGenerateTag(campaign, t);
 
-  const generatedTag: CampaignCardTagProps = useMemo(() => {
-    if (campaignApplication?.state === CampaignApplicationStatus.resolved) {
-      switch (campaignApplication.latestResolution?.status) {
-        case CampaignApplicationResolutionStatus.selected:
-          // calculate time to end of registration by adding registration duration to start time
-          const dateToRegistrationEnd =
-            new Date(campaign?.settings?.openAt).getTime() +
-            campaign?.settings?.registrationPeriodDays * 24 * 60 * 60 * 1000;
+    const customTags: CampaignCardTagProps[] = useMemo(() => {
+        if (!campaign?.tags) {
+            return [];
+        }
+        return campaign.tags.map((tag) => ({
+            children: tag.value,
+            variant: "white",
+        }));
+    }, [campaign]);
 
-          const dateToReviewsEnd =
-            dateToRegistrationEnd +
-            campaign?.settings?.submissionPeriodDays * 24 * 60 * 60 * 1000 +
-            3600000;
+    const tags: CampaignCardTagProps[] = useMemo(() => {
+        return [generatedTag, ...customTags];
+    }, [campaign]);
 
-          const todayMidnight = new Date();
-          todayMidnight.setHours(0, 0, 0, 0);
-          const now = todayMidnight.getTime();
-
-          const reviews =
-            campaign.reviews?.filter((review) => review.state !== ReviewState.draft) || [];
-
-          // Include rejected reviews
-          if (reviews.filter((review) => review.state === ReviewState.rejected).length > 0) {
-            return {
-              children: t("cardStatus.rejected"),
-              variant: "danger",
-            };
-          }
-
-          // Window for reviews is open
-          if (dateToRegistrationEnd <= now && dateToReviewsEnd >= now && reviews.length === 0) {
-            const daysToReviewsEnd = Math.ceil((dateToReviewsEnd - now) / (1000 * 60 * 60 * 24));
-
-            if (daysToReviewsEnd >= 2 && daysToReviewsEnd < 5) {
-              return {
-                children: t("cardStatus.lastDayToReviewLow", {
-                  count: daysToReviewsEnd,
-                }),
-                variant: "danger",
-              };
-            }
-
-            return {
-              children: t("cardStatus.review", {
-                count: daysToReviewsEnd,
-              }),
-              variant: daysToReviewsEnd <= 5 ? "danger" : "white",
-            };
-          }
-
-          // Reviews are submitted
-          if (reviews.length > 0) {
-            return {
-              children: t("cardStatus.reviewed"),
-              variant: "success",
-            };
-          }
-
-          // Its past the time window for entering reviews
-          if (dateToReviewsEnd < now) {
-            const daysPastReviewsEnd = Math.ceil((now - dateToReviewsEnd) / (1000 * 60 * 60 * 24));
-
-            if (daysPastReviewsEnd >= 2 && daysPastReviewsEnd < 5) {
-              return {
-                children: t("cardStatus.missingReviewLow", { count: daysPastReviewsEnd }),
-                variant: "warning",
-                color: "#000000",
-              };
-            }
-
-            return {
-              children: t("cardStatus.missingReview", { count: daysPastReviewsEnd }),
-              variant: "danger",
-              color: "#000000",
-            };
-          }
-
-          return {
-            children: t("cardStatus.selected"),
-            variant: "success",
-          };
-        case CampaignApplicationResolutionStatus.rejected:
-          return {
-            children: t("cardStatus.rejected"),
-            variant: "danger",
-          };
-        default:
-          return {
-            children: t("cardStatus.pending"),
-            variant: "info",
-          };
-      }
-    }
-    if (campaignApplication?.state === CampaignApplicationStatus.submitted) {
-      return {
-        children: t("cardStatus.submitted"),
-        variant: "success",
-      };
-    }
-  }, [campaign]);
-
-  const customTags: CampaignCardTagProps[] = useMemo(() => {
-    if (!campaign?.tags) {
-      return [];
-    }
-    return campaign.tags.map((tag) => ({
-      children: tag.value,
-      variant: "white",
-    }));
-  }, [campaign]);
-
-  const tags: CampaignCardTagProps[] = useMemo(() => {
-    return [generatedTag, ...customTags];
-  }, [campaign]);
-
-  return (
-    <CampaignCard
-      image={image}
-      label={campaign?.name}
-      tags={tags}
-      onClick={onClick}
-      liked={liked}
-      showLikeButton={true}
-      onLikeClick={onLikeClick}
-      onDoubleClick={onDoubleClick}
-    />
-  );
+    return (
+        <CampaignCard
+            image={image}
+            label={campaign?.name}
+            tags={tags}
+            onClick={onClick}
+            liked={liked}
+            showLikeButton={true}
+            onLikeClick={onLikeClick}
+            onDoubleClick={onDoubleClick}
+        />
+    );
 };
