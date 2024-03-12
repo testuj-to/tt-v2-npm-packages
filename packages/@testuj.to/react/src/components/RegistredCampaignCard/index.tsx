af9dcba89c4f1aab116afcb0829150fc3464d775
Petr Hulínský <phulinsky@gmail.com>
2024-03-12 17:24:42 +0100

    fix: remove tags from card preview

--- packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
+++ packages/@testuj.to/react/src/components/RegistredCampaignCard/index.tsx
@@ -1,54 +1,45 @@
 import { useMemo, useState } from "react";
 import { CampaignCard, CampaignCardTagProps } from "../CampaignCard";
-import { Campaign, CampaignApplicationResolutionStatus, CampaignApplicationStatus, ReviewState } from "@lib/types";
+import {
+  Campaign,
+  CampaignApplicationResolutionStatus,
+  CampaignApplicationStatus,
+  ReviewState,
+} from "@lib/types";
 import { useGenerateTag } from "./useGenerateTag";
 
 export interface RegistredCampaignCardProps {
-    campaign: Campaign;
-    onClick?: () => void;
-    onDoubleClick?: () => void;
-    image?: React.ReactNode;
-    liked?: boolean;
-    onLikeClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    t: (key: string, args?: any) => string;
+  campaign: Campaign;
+  onClick?: () => void;
+  onDoubleClick?: () => void;
+  image?: React.ReactNode;
+  liked?: boolean;
+  onLikeClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  t: (key: string, args?: any) => string;
 }
 
 export const RegistredCampaignCard = ({
-    campaign,
-    onClick,
-    image,
-    onDoubleClick,
-    liked,
-    onLikeClick,
-    t,
+  campaign,
+  onClick,
+  image,
+  onDoubleClick,
+  liked,
+  onLikeClick,
+  t,
 }: RegistredCampaignCardProps) => {
-    const generatedTag = useGenerateTag(campaign, t);
+  const generatedTag = useGenerateTag(campaign, t);
 
-    const customTags: CampaignCardTagProps[] = useMemo(() => {
-        if (!campaign?.tags) {
-            return [];
-        }
-        return campaign.tags.map((tag) => ({
-            children: tag.value,
-            variant: "white",
-        }));
-    }, [campaign]);
-
-    const tags: CampaignCardTagProps[] = useMemo(() => {
-        return [generatedTag, ...customTags];
-    }, [campaign]);
-
-    return (
-        <CampaignCard
-            image={image}
-            label={campaign?.name}
-            tags={tags}
-            onClick={onClick}
-            liked={liked}
-            showLikeButton={true}
-            onLikeClick={onLikeClick}
-            onDoubleClick={onDoubleClick}
-            communityLogo={campaign?.mainTenant?.logo?.src}
-        />
-    );
+  return (
+    <CampaignCard
+      image={image}
+      label={campaign?.name}
+      tags={[generatedTag]}
+      onClick={onClick}
+      liked={liked}
+      showLikeButton={true}
+      onLikeClick={onLikeClick}
+      onDoubleClick={onDoubleClick}
+      communityLogo={campaign?.mainTenant?.logo?.src}
+    />
+  );
 };
