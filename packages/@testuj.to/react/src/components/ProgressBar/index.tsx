c2d7b7750446f0b344ae26ff9bf00528dabdf2dc
Petr Hulínský <phulinsky@gmail.com>
2024-05-09 14:23:37 +0200

    feat: tooltip, update progress bar

--- packages/@testuj.to/react/src/components/ProgressBar/index.tsx
+++ packages/@testuj.to/react/src/components/ProgressBar/index.tsx
@@ -2,51 +2,82 @@ import { useEffect, useState } from "react";
 import cx from "classnames";
 
 import "./styles.css";
+import { Tooltip } from "../Tooltip";
 
 export interface ProgressBarProps {
-  value: number;
-  className?: string;
-  delay?: number;
-  breakpoints?: number[];
-  finalIcon?: React.ReactNode;
-  backgroundColor?: string;
-  lineColor?: string;
-  breakpointColor?: string;
+    value: number;
+    className?: string;
+    delay?: number;
+    breakpoints?: number[] | Breakpoint[];
+    finalIcon?: React.ReactNode;
+    backgroundColor?: string;
+    lineColor?: string;
+    breakpointColor?: string;
+}
+
+export interface Breakpoint {
+    value: number;
+    color?: string;
+    component?: React.ReactNode;
+    componentSuccess?: React.ReactNode;
+    tooltip?: React.ReactNode;
 }
 
 export const ProgressBar = ({
-  value,
-  delay,
-  className,
-  breakpoints,
-  finalIcon,
-  backgroundColor,
-  lineColor,
-  breakpointColor,
+    value,
+    delay,
+    className,
+    breakpoints,
+    finalIcon,
+    backgroundColor,
+    lineColor,
+    breakpointColor,
 }: ProgressBarProps) => {
-  const [progress, setProgress] = useState(delay ? 0 : value);
+    const [progress, setProgress] = useState(delay ? 0 : value);
 
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(value), delay || 0);
-    return () => clearTimeout(timer);
-  }, [value]);
+    useEffect(() => {
+        const timer = setTimeout(() => setProgress(value), delay || 0);
+        return () => clearTimeout(timer);
+    }, [value]);
 
-  return (
-    <div className={cx("tt-progress-bar-wrapper", className)}>
-      <div className={cx("tt-progress-bar")} style={{ backgroundColor: backgroundColor }}>
-        {breakpoints?.map((breakpoint) => (
-          <div
-            className="tt-progress-bar-breakpoint"
-            style={{ left: `${breakpoint}%`, backgroundColor: breakpointColor }}
-            key={breakpoint}
-          />
-        ))}
-        <div
-          className="tt-progress-bar-indicator"
-          style={{ width: `${progress}%`, backgroundColor: lineColor }}
-        />
-      </div>
-      <div className="tt-progress-bar-icon">{finalIcon}</div>
-    </div>
-  );
+    return (
+        <div className={cx("tt-progress-bar-wrapper", className)}>
+            <div className={cx("tt-progress-bar")} style={{ backgroundColor: backgroundColor }}>
+                {breakpoints?.map((breakpoint: number | Breakpoint, index) => (
+                    <>
+                        {typeof breakpoint === "number" ? (
+                            <div
+                                className="tt-progress-bar-breakpoint"
+                                style={{ left: `${breakpoint}%`, backgroundColor: breakpointColor }}
+                                key={breakpoint + index}
+                            ></div>
+                        ) : (
+                            <div
+                                className="tt-progress-bar-breakpoint-component"
+                                style={{
+                                    left: `${breakpoint.value}%`,
+                                    backgroundColor: breakpoint.color,
+                                }}
+                                key={breakpoint.value + index}
+                            >
+                                <Tooltip content={breakpoint.tooltip}>
+                                    {progress >= breakpoint.value
+                                        ? breakpoint.componentSuccess || breakpoint.component
+                                        : breakpoint.component}
+                                </Tooltip>
+                            </div>
+                        )}
+                    </>
+                ))}
+                <div
+                    className="tt-progress-bar-indicator"
+                    style={{
+                        width: `${progress > 100 ? 100 : progress}%`,
+                        backgroundColor: lineColor,
+                    }}
+                />
+            </div>
+            {finalIcon && <div className="tt-progress-bar-icon">{finalIcon}</div>}
+        </div>
+    );
 };
