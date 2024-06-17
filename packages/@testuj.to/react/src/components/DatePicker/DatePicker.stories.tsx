d18d3ef4106cfa945f9a884c7ce5bd8a0a899db7
Petr Hulínský <phulinsky@gmail.com>
2024-06-17 17:26:12 +0200

    fix: mobile date picker

--- packages/@testuj.to/react/src/components/DatePicker/DatePicker.stories.tsx
+++ packages/@testuj.to/react/src/components/DatePicker/DatePicker.stories.tsx
@@ -5,30 +5,28 @@ import { DatePicker } from ".";
 import { useState } from "react";
 
 export default {
-  component: DatePicker,
-  title: "Primitives/DatePicker",
-  parameters: {
-    layout: "centered",
-  },
-  decorators: [TTContextDecorator],
+    component: DatePicker,
+    title: "Primitives/DatePicker",
+    parameters: {
+        layout: "centered",
+    },
+    decorators: [TTContextDecorator],
 } as ComponentMeta<typeof DatePicker>;
 
 const datePickerStoryTemplate: ComponentStory<typeof DatePicker> = (args) => {
-  const [selected, setSelected] = useState<Date | null>(new Date());
-
-  console.log(selected);
-
-  return (
-    <DatePicker {...args} selected={selected} onChange={(date) => setSelected(date as Date)} />
-  );
+    const [selected, setSelected] = useState<Date | null>(new Date());
+    return (
+        <DatePicker {...args} selected={selected} onChange={(date) => setSelected(date as Date)} />
+    );
 };
 
 export const Basic = datePickerStoryTemplate.bind({});
 Basic.args = {
-  translationFunciton: (key: string) => key,
-  onChange: (dateRange: [Date | null, Date | null]) => console.log(dateRange),
-  type: "single",
-  selected: new Date(),
-  dateFormat: "dd.MM.yyyy HH:mm",
-  dateTime: true,
+    translationFunciton: (key: string) => key,
+    onChange: (dateRange: [Date | null, Date | null]) => console.log(dateRange),
+    type: "single",
+    selected: new Date(),
+    // dateRange: [new Date(new Date().getTime() - 1000000000), new Date()],
+    dateFormat: "dd.MM.yyyy HH:mm",
+    // dateTime: true,
 };
