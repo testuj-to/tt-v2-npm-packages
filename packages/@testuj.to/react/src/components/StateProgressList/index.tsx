5263af67a162d13f4da47b0f81e3f473c5f8c6d2
Patrik Šimunič <patriksimunic@hotmail.com>
2024-08-01 11:10:43 +0200

    feat: v0.0.170

--- packages/@testuj.to/react/src/components/StateProgressList/index.tsx
+++ packages/@testuj.to/react/src/components/StateProgressList/index.tsx
@@ -2,30 +2,23 @@ import cx from "classnames";
 
 import "./styles.css";
 
-import { IconPending } from "./IconPending";
-import { IconCheck } from "./IconCheck";
-import { IconCancel } from "./IconCancel";
+import { Item, ProgressState } from "./item";
 
-export enum ProgressState {
-    active = "active",
-    done = "done",
-    disabled = "disabled",
-    error = "error",
-}
+export { ProgressState } from "./item";
 
-export type StateProgressListItem = {
+export interface StateProgressListItem {
     state?: ProgressState;
+    active?: boolean;
     content?: React.ReactNode;
     note?: React.ReactNode;
-    active?: boolean;
-};
+}
 
 export interface StateProgressListProps {
     className?: string;
     items: StateProgressListItem[];
     currentIndex?: number;
     pastSuccess?: boolean;
-    onClick?: (index: number) => void;
+    onClick?(index: number): void;
 }
 
 export const StateProgressList = ({
@@ -46,7 +39,7 @@ export const StateProgressList = ({
                 }
 
                 return (
-                    <StateProgressListItem
+                    <Item
                         key={index}
                         state={currentState}
                         note={note}
@@ -54,52 +47,9 @@ export const StateProgressList = ({
                             onClick?.(index)}
                     >
                         {content}
-                    </StateProgressListItem>
+                    </Item>
                 );
             })}
         </ol>
-    );
-};
-
-interface StateProgressListItemProps {
-    state: ProgressState;
-    note?: React.ReactNode;
-    children?: React.ReactNode;
-    onClick?: () => void;
-}
-
-const StateProgressListItem = ({ state, children, onClick, note }: StateProgressListItemProps) => {
-    return (
-        <>
-            <li
-                className="tt-state-progress-list-item"
-                onClick={onClick}
-            >
-                <StateProgressIndicator {...{ state }} />
-                    <div className={cx("tt-state-progress-list-item_content", state)}>
-                        {children}
-                    </div>
-            </li>
-            {note}
-        </>
-    );
-};
-
-interface StateProgressIndicatorProps {
-    state: ProgressState;
-}
-
-const StateProgressIndicator = ({ state }: StateProgressIndicatorProps) => {
-    const Icon = {
-        active: <IconPending/>,
-        done: <IconCheck/>,
-        disabled: <IconCancel/>,
-        error: <IconCancel/>,
-    }[state];
-
-    return (
-        <div className={cx("tt-state-progress-indicator", state)}>
-            {Icon}
-        </div>
     );
 };
